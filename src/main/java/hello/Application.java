88adbd3aa49fb64199701d7393a99034ad043f3a
Charles-Chan-CCSC <Charles.cs.chan.pccw@gmail.com>
2022-06-17 15:42:37 +0800

    test to print sth

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -55,7 +55,31 @@ public class Application {
 
   @PostMapping("/**")
   public String index(@RequestBody ArenaUpdate arenaUpdate) {
-    System.out.println(arenaUpdate);
+    System.out.println("kkkkkkkk");
+
+
+/*
+    String jsonString = "{\"stat\": { \"sdr\": \"aa:bb:cc:dd:ee:ff\", \"rcv\": \"aa:bb:cc:dd:ee:ff\", \"time\": \"UTC in millis\", \"type\": 1, \"subt\": 1, \"argv\": [{\"type\": 1, \"val\":\"stackoverflow\"}]}}";
+    JSONObject jsonObject = new JSONObject(jsonString);
+    JSONObject newJSON = jsonObject.getJSONObject("arena");
+    System.out.println(newJSON.toString());
+    jsonObject = new JSONObject(newJSON.toString());
+    System.out.println(jsonObject.getString("rcv"));
+    System.out.println(jsonObject.getJSONArray("argv"));
+*/
+
+    //Our chess
+
+    String me = "https://cloud-run-hackathon-java-springboot-ksvehvegcq-uc.a.run.app";
+
+    int myX = arenaUpdate.arena.state.get(me).x;
+    int myY = arenaUpdate.arena.state.get(me).y;
+    String myDirection = arenaUpdate.arena.state.get(me).direction;
+    boolean wasHit = arenaUpdate.arena.state.get(me).wasHit;
+    int score = arenaUpdate.arena.state.get(me).score;
+
+    System.out.println(myX+"   "+myY);
+
     String[] commands = new String[]{"F", "R", "L", "T"};
     int i = new Random().nextInt(4);
     return commands[i];
