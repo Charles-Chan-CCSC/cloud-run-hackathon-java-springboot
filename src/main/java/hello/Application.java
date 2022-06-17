e872baf92d379ca91c4b2421c4ea3b01e33234a0
Charles-Chan-CCSC <Charles.cs.chan.pccw@gmail.com>
2022-06-17 16:24:31 +0800

    Add json, and test on getting my x and y

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,5 +1,6 @@
 package hello;
 
+import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.WebDataBinder;
@@ -27,16 +28,52 @@ public class Application {
     public String direction;
     public Boolean wasHit;
     public Integer score;
+
+    public Integer getX() {return x;}
+
+    public void setX(Integer x) {this.x = x;}
+
+    public Integer getY() {return y;}
+
+    public void setY(Integer y) {this.y = y;}
+
+    public String getDirection() {return direction;}
+
+    public void setDirection(String direction) {this.direction = direction;}
+
+    public Boolean getWasHit() {return wasHit;}
+
+    public void setWasHit(Boolean wasHit) {this.wasHit = wasHit;}
+
+    public Integer getScore() {return score;}
+
+    public void setScore(Integer score) {this.score = score;}
   }
 
   static class Arena {
     public List<Integer> dims;
     public Map<String, PlayerState> state;
+
+    public List<Integer> getDims() {return dims;}
+
+    public void setDims(List<Integer> dims) {this.dims = dims;}
+
+    public Map<String, PlayerState> getState() {return state;}
+
+    public void setState(Map<String, PlayerState> state) {this.state = state;}
   }
 
   static class ArenaUpdate {
     public Links _links;
     public Arena arena;
+
+    public Links get_links() {return _links;}
+
+    public void set_links(Links _links) {this._links = _links;}
+
+    public Arena getArena() {return arena;}
+
+    public void setArena(Arena arena) {this.arena = arena;}
   }
 
   public static void main(String[] args) {
@@ -67,22 +104,29 @@ public class Application {
     System.out.println(jsonObject.getJSONArray("argv"));
 */
 
-    //Our chess
+    JSONObject jsonObject = new JSONObject(arenaUpdate);
+
+    JSONObject arena = jsonObject.getJSONObject("arena");
+    System.out.println(arena.toString());
+
+    JSONObject state = jsonObject.getJSONObject("state");
+    System.out.println(state.toString());
 
     String me = "https://cloud-run-hackathon-java-springboot-ksvehvegcq-uc.a.run.app";
+    JSONObject meJson = jsonObject.getJSONObject(me);
+    int myX = meJson.getInt("x");
+    int myY = meJson.getInt("y");
 
-    int myX = arenaUpdate.arena.state.get(me).x;
-    int myY = arenaUpdate.arena.state.get(me).y;
+    System.out.println("hhhhhhhhhhhhhh");
+    System.out.println(myX+"   "+myY);
+    System.out.println("hhhhhhhhhhhhhh");
+
+    /*
     String myDirection = arenaUpdate.arena.state.get(me).direction;
     boolean wasHit = arenaUpdate.arena.state.get(me).wasHit;
     int score = arenaUpdate.arena.state.get(me).score;
-
-    System.out.println("111");
-
-    System.out.println(arenaUpdate);
-    System.out.println(myX+"   "+myY);
-
-    System.out.println("222");
+*/
+    
 
     String[] commands = new String[]{"F", "R", "L", "T"};
     int i = new Random().nextInt(4);
